7331852dc773323fb8e0b9865376b5d606f65231
j0rdanaaa <j0rdanaaa@gmail.com>
2023-06-23 16:52:28 -0300

    exercícios feitos

--- src/components/ListaTarefas/index.js
+++ src/components/ListaTarefas/index.js
@@ -1,60 +1,88 @@
-import { useState } from "react";
+import { useState } from 'react';
 import {
-  InputContainer,
-  ListaContainer,
-  ListaTarefasContainer,
-  Tarefa,
-  TaskInput,
-  AddTaskButton,
-  RemoveButton,
-  LinhaHorizontal
-} from "./styled";
-import bin from "../../assets/bin.png";
+    InputContainer,
+    ListaContainer,
+    ListaTarefasContainer,
+    Tarefa,
+    TaskInput,
+    AddTaskButton,
+    RemoveButton,
+    LinhaHorizontal,
+} from './styled';
+import bin from '../../assets/bin.png';
+import ListaTarefasConcluidas from '../ListaTarefaConcluida';
 
 export function ListaTarefas() {
-  const [lista, setLista] = useState(["Fazer exercícios", "Estudar React"]);
-  const [novaTarefa, setNovaTarefa] = useState("");
+    const [lista, setLista] = useState(['Fazer exercícios', 'Estudar React']);
+    const [novaTarefa, setNovaTarefa] = useState('');
+    const onChangeTarefa = (event) => {
+        setNovaTarefa(event.target.value);
+    };
+    const adicionaTarefa = () => {
+        const novaLista = [...lista, novaTarefa];
+        setLista(novaLista);
+        setNovaTarefa('');
+    };
+    // Adicionando tarefas com o btn Enter
+    const adicionaTarefaComEnter = (event) => {
+        if (event.keyCode === 13) {
+            adicionaTarefa();
+        }
+    };
 
-  const onChangeTarefa = (event) => {
-    setNovaTarefa(event.target.value);
-  };
+    // Adicionando tarefa concluída a lista de tarefas concluídas:
+    // estado
+    const [itemConcluido, setItemConcluido] = useState([]);
 
-  const adicionaTarefa = () => {
-    const novaLista = [...lista, novaTarefa];
-    setLista(novaLista);
-    setNovaTarefa("");
-  };
+    // função
+    const adicionaTarefaConcluida = (tarefa) => {
+        const listaTarefasConcluidas = lista.filter((item) => item === tarefa);
+        const novaListaTarefasConcluidas = [
+            ...itemConcluido,
+            listaTarefasConcluidas,
+        ];
+        setItemConcluido(novaListaTarefasConcluidas);
+    };
 
-  const removeTarefa = (tarefa) => {
-    const listaFiltrada = lista.filter((item) => item !== tarefa);
-    setLista(listaFiltrada);
-  };
+    const removeTarefa = (tarefa) => {
+        // capturando tarefa concluída:
+        adicionaTarefaConcluida(tarefa);
 
-  return (
-    <ListaTarefasContainer>
-      <InputContainer>
-        <TaskInput
-          placeholder="Digite aqui uma tarefa"
-          value={novaTarefa}
-          onChange={onChangeTarefa}
-        />
-        <AddTaskButton onClick={adicionaTarefa}>Adicionar</AddTaskButton>
-      </InputContainer>
-      <ListaContainer>
-        <ul>
-          {lista.map((tarefa, index) => {
-            return (
-              <Tarefa key={index}>
-                <p>{tarefa}</p>
-                <RemoveButton onClick={() => removeTarefa(tarefa)}>
-                  <img src={bin} alt="" width="16px" />
-                </RemoveButton>
-              </Tarefa>
-            );
-          })}
-        </ul>
-      </ListaContainer>
-      <LinhaHorizontal/>
-    </ListaTarefasContainer>
-  );
+        //lógica da remoção de tarefa:
+        const listaFiltrada = lista.filter((item) => item !== tarefa);
+        setLista(listaFiltrada);
+    };
+    return (
+        <ListaTarefasContainer>
+            <InputContainer>
+                <TaskInput
+                    placeholder="Digite aqui uma tarefa"
+                    value={novaTarefa}
+                    onChange={onChangeTarefa}
+                    onKeyDown={(event) => adicionaTarefaComEnter(event)}
+                />
+                <AddTaskButton onClick={adicionaTarefa}>
+                    Adicionar
+                </AddTaskButton>
+            </InputContainer>
+            <ListaContainer>
+                <ul>
+                    {lista.map((tarefa, index) => {
+                        return (
+                            <Tarefa key={index}>
+                                <p>{tarefa}</p>
+                                <RemoveButton
+                                    onClick={() => removeTarefa(tarefa)}
+                                >
+                                    <img src={bin} alt="" width="16px" />
+                                </RemoveButton>
+                            </Tarefa>
+                        );
+                    })}
+                </ul>
+            </ListaContainer>
+            <LinhaHorizontal />
+            <ListaTarefasConcluidas itemConcluido={itemConcluido} />
+        </ListaTarefasContainer>
+    );
 }
